b7b9b4f4e29b3809e82306148b5a299e61898fb0
Alex Kindel <atkindel@stanford.edu>
2015-09-29 16:21:11 -0700

    extractQualtrics method from other ETL process

--- manageProjectData.py
+++ manageProjectData.py
@@ -4,6 +4,10 @@ import urllib2
 import os.path
 import sys
 import ConfigParser
+import json
+import StringIO as sio
+import zipfile as z
+from pypodio2 import api
 
 ### Script automatically refreshes Podio projects and consultation data
 ### Pulls from Qualtrics API
@@ -63,12 +67,68 @@ class ProjectDataManager(object):
 
     def extractQualtrics(self):
         '''
-        Pull form data down from Qualtrics
+        Pull form data down from Qualtrics.
         '''
-        print self.qtUser, self.qtToken, self.pdETL, self.pdKey, self.pdApp
+
+        idRRF = 'SV_78KTbL61clEWsO9'
+        idPRF = 'SV_bftcKQJ9cGUyPI1'
+
+        dataRRF = self.__getFormData(idRRF)
+        dataPRF = self.__getFormData(idPRF)
+
+        return dataRRF, dataPRF
+        
+
+    def __getFormData(self, surveyID):
+        '''
+        Pull PRF form data down from Qualtrics. From qualtrics_etl.
+        Returns JSON object containing untransformed survey data.
+        '''
+
+        urlTemp = Template("https://dc-viawest.qualtrics.com:443/API/v1/surveys/${svid}/responseExports?apiToken=${tk}&fileType=JSON")
+        reqURL = urlTemp.substitute(svid=surveyID, tk=self.qtToken)
+        req = json.loads(urllib2.urlopen(reqURL).read())
+
+        statURL = req['result']['exportStatus'] + "?apiToken=" + self.qtToken
+        percent, tries = 0, 0
+        while percent != 100 and tries < 20:
+            time.sleep(5) # Wait 5 seconds between attempts to acquire data
+            try:
+                stat = json.loads(urllib2.urlopen(statURL).read())
+                percent = stat['result']['percentComplete']
+            except:
+                print "Extractor recovered from HTTP error."
+                continue
+            finally:
+                tries += 1
+        if tries >= 20:
+            logging.error("Survey %s timed out." % surveyID)
+            return None
+
+        dataURL = stat['result']['fileUrl']
+        remote = urllib2.urlopen(dataURL).read()
+        dataZip = sio.StringIO(remote)
+        archive = z.ZipFile(dataZip, 'r')
+        dataFile = archive.namelist()[0]
+        data = json.loads(archive.read(dataFile), object_pairs_hook=OrderedDict)
+
+        if not data['responses']:
+            return None
+        else:
+            return data
 
 
 
 if __name__ == '__main__':
     pdm = ProjectDataManager()
-    pdm.extractQualtrics()
+    rrf, prf = pdm.extractQualtrics()
+    try:
+        print rrf
+    except:
+        print "rrf failed"
+        continue
+    try:
+        print prf
+    except:
+        print "prf failed"
+        continue
