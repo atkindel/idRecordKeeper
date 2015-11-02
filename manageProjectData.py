af56c23de1e1e77e8efbbe9b57d0897c749ee82f
Alex Kindel <atkindel@stanford.edu>
2015-11-02 12:55:14 -0800

    Print out problematic dict field

--- manageProjectData.py
+++ manageProjectData.py
@@ -178,6 +178,7 @@ class ProjectDataManager(object):
             if (parsedProj['course-offering-type.text'] == 'Derivative' or parsedProj['course-offering-type.text'] == 'First Run'):
                 audience = "[%s]: %s" % (date, rawProj.pop('Q35'))
                 parsedProj['audience-notes'] = audience
+                print "Audience notes: %s" % parsedProj['audience-notes']
                 parsedProj['short-description'] += "<p><b>Project description:</b> %s<br/><br/><b>Impact:</b> %s<br/><br/><b>Support needed:</b> %s<br/><br/><b>Research/evaluation plans:</b> %s<br/><br/><b>Schedule:</b> %s</p>" % (rawProj.pop('Q15'), rawProj.pop('Q16'), rawProj.pop('Q17'), rawProj.pop('Q20'), rawProj.pop('Q21'))
                 parsedProj['funding-stipulations'] = rawProj.pop('Q36')
                 parsedProj['consult'] = rawProj.pop('Q18', "No one")
